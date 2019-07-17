a0c4d1b9c3fb8d4c74b3bbf264fb63cd435f5b0c
WajeehaParker <wajeeha.parker@gmail.com>
2019-07-18 01:14:56 +0500

    uploading

---
+++ src/components/Highlights/Description.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+
+const Description = () => {
+  return(
+    <Fade>
+      <div className="center_wrapper">
+        <h2>Highlights</h2>
+        <div className="highlight_description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat, quam at interdum sodales, orci purus dignissim libero, nec feugiat ipsum ligula in mi. Ut sit amet justo velit. Sed id quam odio. Nullam ut mauris id metus porta vestibulum sed ac ex. Suspendisse porttitor feugiat tortor gravida facilisis. Nam fermentum, sapien sed sodales laoreet, odio nisl ultrices ligula, vitae porta mi diam a erat.
+        </div>
+      </div>
+    </Fade>
+  );
+}
+
+export default Description;
